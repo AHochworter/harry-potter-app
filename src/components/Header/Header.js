3150553817649a2ef44b9a1035ed98ebe98862b0
Ann Hochworter <ann.hochworter@gmail.com>
2023-11-07 15:49:54 -0700

    Mess with getting the home link somewhat where I want it

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,14 +5,18 @@ function Header() {
   const location = useLocation();
 
   return (
-    <div className="header">
-      <h1 className="heading-one">Harry Potter</h1>
-      <h2 className="heading-two">Magical Moments</h2>
-      {location.pathname !== '/' && (
-        <Link className="home-link" to="/">
-          Home
-        </Link>
-      )}
+    <div className="header-container">
+      <div className="app-title-container">
+        <h1 className="heading-one">Harry Potter</h1>
+        <h2 className="heading-two">Magical Moments</h2>
+      </div>
+      <div className="header">
+        {location.pathname !== '/' && (
+          <Link className="home-link" to="/">
+            Home
+          </Link>
+        )}
+      </div>
     </div>
   );
 }
