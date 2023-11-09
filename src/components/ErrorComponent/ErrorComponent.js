c50b5962acaa375d35050b7438bd1c99d8b82514
Ann Hochworter <ann.hochworter@gmail.com>
2023-11-09 13:50:06 -0700

    Add error messages for user empathy

--- src/components/ErrorComponent/ErrorComponent.js
+++ src/components/ErrorComponent/ErrorComponent.js
@@ -1,10 +1,16 @@
 import './ErrorComponent.css';
 import PropTypes from 'prop-types';
+import Fluffy from '../../images/harry-potter-fluffy.jpg';
 
 function ErrorComponent({ error, message }) {
   return (
     <div className="error-container">
       <h2 className="error-h2">ERROR</h2>
+      <img
+        className="error-image"
+        src={Fluffy}
+        alt="harry potter fluffy dog image"
+      />
       <h3 className="error-h3">{error}</h3>
       <h3 className="error-h3">{message}</h3>
     </div>
